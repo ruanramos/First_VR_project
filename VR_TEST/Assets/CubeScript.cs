1d08585cfc810479cfb7714aed625038a02971a5
Ruan Ramos <ruan.ramos28@gmail.com>
2019-06-10 02:00:19 -0300

    First commit. VR is working in editor, but can't build to android, don't know why yet

---
+++ VR_TEST/Assets/CubeScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+
+    private float timer;
+    private bool shouldChangeColor;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (shouldChangeColor)
+        {
+            CheckTimer();    
+        }
+    }
+
+    private void CheckTimer()
+    {
+        if (Time.time - timer > 2f)
+        {
+            ChangeColor();
+            shouldChangeColor = false;
+        }
+    }
+
+    public void ChangeColor()
+    {
+        var color = new Color(Random.value, Random.value, Random.value);
+        gameObject.GetComponent<Renderer>().material.color = color;
+    }
+
+    public void Entered()
+    {
+        StartTimer();
+        shouldChangeColor = true;
+    }
+
+    public void Exit()
+    {
+        timer = 0;
+        shouldChangeColor = false;
+    }
+
+    private void StartTimer()
+    {
+        timer = Time.time;
+    }
+}
